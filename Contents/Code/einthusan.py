63e1a3403908b6e966ca44da740eaa4832a9f4e0
ngovil21 <ngovil21@gmail.com>
2015-06-07 20:57:25 -0700

    fixes

--- Contents/Code/einthusan.py
+++ Contents/Code/einthusan.py
@@ -12,39 +12,41 @@ NAME = common.NAME
 ART = common.ART
 ICON = common.ICON
 
+
 @route(PREFIX + '/einthusan/languages')
 def LanguagesMenu(url):
-
     oc = ObjectContainer(title2=SITETITLE)
 
     oc.add(DirectoryObject(key=Callback(MainMenu, url=url + 'index.php?lang=hindi', title='Hindi'), title='Hindi'))
     oc.add(DirectoryObject(key=Callback(MainMenu, url=url + 'index.php?lang=tamil', title='Tamil'), title='Tamil'))
     oc.add(DirectoryObject(key=Callback(MainMenu, url=url + 'index.php?lang=telegu', title='Telegu'), title='Telegu'))
-    oc.add(DirectoryObject(key=Callback(MainMenu, url=url + 'index.php?lang=malayalam', title='Malayalam'), title='Malayalam'))
+    oc.add(DirectoryObject(key=Callback(MainMenu, url=url + 'index.php?lang=malayalam', title='Malayalam'),
+                           title='Malayalam'))
 
     return oc
 
 
 @route(PREFIX + '/einthusan/main')
-def MainMenu(url,title):
-
+def MainMenu(url, title):
     oc = ObjectContainer(title2=title)
 
     oc.add(DirectoryObject(key=Callback(ShowcaseMenu, url=url, title='Showcase'), title='Showcase'))
     oc.add(DirectoryObject(key=Callback(HDMenu, url=url, title='HD Movies', language=title.lower()), title='HD Movies'))
-    oc.add(DirectoryObject(key=Callback(BlurayMenu, url=url, title='Blu-ray Movies', language=title.lower()), title='Blu-ray Movies'))
-    oc.add(InputDirectoryObject(key=Callback(SearchResultsMenu, language=title.lower()), title='Search', prompt="Enter the name of the Movie to search:"))
-    #oc.add(DirectoryObject(key=Callback(SearchInputMenu, title='Search', language=title), title='Search'))
+    oc.add(DirectoryObject(key=Callback(BlurayMenu, url=url, title='Blu-ray Movies', language=title.lower()),
+                           title='Blu-ray Movies'))
+    oc.add(InputDirectoryObject(key=Callback(SearchResultsMenu, language=title.lower()), title='Search',
+                                prompt="Enter the name of the Movie to search:"))
+    # oc.add(DirectoryObject(key=Callback(SearchInputMenu, title='Search', language=title), title='Search'))
     return oc
 
-@route(PREFIX + '/einthusan/showcase')
-def ShowcaseMenu(url,title):
 
+@route(PREFIX + '/einthusan/showcase')
+def ShowcaseMenu(url, title):
     oc = ObjectContainer(title2=title)
     html = HTML.ElementFromURL(url)
 
     for item in html.xpath("//div[@id='movie-showcase-slides']/div"):
-        #try:
+        # try:
         link = item.xpath(".//a[@class='movie-title']")[0]
         url = link.xpath("./@href")[0].lstrip(' .')
         if not url.startswith("http://"):
@@ -67,8 +69,9 @@ def ShowcaseMenu(url,title):
 
     return oc
 
+
 @route(PREFIX + '/einthusan/HdMovies')
-def HDMenu(url,title, language):
+def HDMenu(url, title, language):
     oc = ObjectContainer(title2=title)
 
     url = "http://einthusan.com/movies/index.php?lang=" + language
@@ -79,7 +82,7 @@ def HDMenu(url,title, language):
 
 
 @route(PREFIX + '/einthusan/BlurayMovies')
-def BlurayMenu(url,title,language):
+def BlurayMenu(url, title, language):
     oc = ObjectContainer(title2=title)
 
     url = "http://einthusan.com/bluray/index.php?lang=" + language
@@ -88,8 +91,9 @@ def BlurayMenu(url,title,language):
 
     return oc
 
+
 @route(PREFIX + '/einthusan/Year')
-def YearMenu(url,title):
+def YearMenu(url, title):
     oc = ObjectContainer(title2=title)
     yurl = url + "&organize=Year"
 
@@ -110,6 +114,7 @@ def YearMenu(url,title):
 
     return oc
 
+
 @route(PREFIX + '/einthusan/MovieList')
 def MovieListMenu(url, title):
     oc = ObjectContainer(title2=title)
@@ -118,7 +123,7 @@ def MovieListMenu(url, title):
 
     for item in html.xpath("//div[@class='video-object-wrapper']"):
         x_image = item.xpath("./a//img")
-        thumb=None
+        thumb = None
         if x_image:
             image = x_image[0].xpath("./@src")[0].lstrip(". ")
             if not image.startswith("http:"):
@@ -131,6 +136,28 @@ def MovieListMenu(url, title):
         title = x_link.xpath("./text()")[0]
         oc.add(DirectoryObject(key=Callback(PlayMovie, url=link, title=title), title=title, thumb=thumb))
 
+    pages = html.xpath(".//div[@id='content']/div[@class='numerical-nav']/a")
+    if len(pages) > 1:
+        prevPage = ""
+        nextPage = ""
+        if url.contains("movies"):
+            baseURL = "http://einthusan.com/movies/index.php"
+        elif url.contains("blueray"):
+            baseURL  = http://einthusan.com/bluray/index.php
+        for i in range(0,len(pages)):
+            p = pages[i]
+            if p.xpath("./@class='numerical-nav-selected'"):
+                if i > 0:
+                    prevPage = pages[i-1].xpath("./@href")[0]
+                    if not prevPage.startswith("http:"):
+                        prevPage = baseURL + prevPage
+                    oc.add(DirectoryObject(key=Callback(MovieListMenu, url=prevPage, title=title), title="Previous Page"))
+                if i < (len(pages)-1):
+                    nextPage = pages[i+1].xpath("./@href")[0]
+                    if not nextPage.startswith("http:"):
+                        nextPage = baseURL + nextPage
+                    oc.add(DirectoryObject(key=Callback(MovieListMenu, url=nextPage, title=title), title="Next Page"))
+                break
 
     if len(oc) == 0:
         return ObjectContainer(header=title, message=L('ShowWarning'))
@@ -140,15 +167,15 @@ def MovieListMenu(url, title):
 
 @route(PREFIX + '/einthusan/searchinput')
 def SearchInputMenu(title, language):
-
     oc = ObjectContainer(title2=title)
 
-    oc.add(InputDirectoryObject(key=Callback(SearchResultsMenu, language=language), title=title, prompt="Enter the name of the Movie to search:"))
+    oc.add(InputDirectoryObject(key=Callback(SearchResultsMenu, language=language), title=title,
+                                prompt="Enter the name of the Movie to search:"))
     return oc
+
 
 @route(PREFIX + '/einthusan/searchresults')
 def SearchResultsMenu(language, query):
-
     oc = ObjectContainer(title2="Search")
 
     query = String.Quote(query, usePlus=True)
@@ -171,9 +198,9 @@ def SearchResultsMenu(language, query):
 
     return oc
 
-@route(PREFIX + '/einthusan/movie')
-def PlayMovie(url,title):
 
+@route(PREFIX + '/einthusan/movie')
+def PlayMovie(url, title):
     oc = ObjectContainer(title2=title)
     html = HTML.ElementFromURL(url)
 
@@ -191,45 +218,46 @@ def PlayMovie(url,title):
 
     return oc
 
+
 @route(PREFIX + '/einthusan/createvideoobject')
-def CreateVideoObject(url, title, thumb, originally_available_at=None,summary=None, include_container=False):
-  try:
-    originally_available_at = Datetime.ParseDate(originally_available_at).date()
-  except:
-    originally_available_at = None
+def CreateVideoObject(url, title, thumb, originally_available_at=None, summary=None, include_container=False):
+    try:
+        originally_available_at = Datetime.ParseDate(originally_available_at).date()
+    except:
+        originally_available_at = None
 
-  container = Container.MP4
-  video_codec = VideoCodec.H264
-  audio_codec = AudioCodec.AAC
-  audio_channels = 2
+    container = Container.MP4
+    video_codec = VideoCodec.H264
+    audio_codec = AudioCodec.AAC
+    audio_channels = 2
 
-  video_object = VideoClipObject(
-    key = Callback(
-      CreateVideoObject,
-      url=url,
-      title=title,
-      thumb=thumb,
-      originally_available_at=originally_available_at,
-      include_container=True
-    ),
-    rating_key = url,
-    title = title,
-    thumb=thumb,
-    originally_available_at=originally_available_at,
-    items = [
-      MediaObject(
-        parts = [
-          PartObject(key=url)
-        ],
-        container = container,
-        video_codec = video_codec,
-        audio_codec = audio_codec,
-        audio_channels = audio_channels
-      )
-    ]
-  )
+    video_object = VideoClipObject(
+        key=Callback(
+            CreateVideoObject,
+            url=url,
+            title=title,
+            thumb=thumb,
+            originally_available_at=originally_available_at,
+            include_container=True
+        ),
+        rating_key=url,
+        title=title,
+        thumb=thumb,
+        originally_available_at=originally_available_at,
+        items=[
+            MediaObject(
+                parts=[
+                    PartObject(key=url)
+                ],
+                container=container,
+                video_codec=video_codec,
+                audio_codec=audio_codec,
+                audio_channels=audio_channels
+            )
+        ]
+    )
 
-  if include_container:
-    return ObjectContainer(objects=[video_object])
-  else:
-    return video_object
+    if include_container:
+        return ObjectContainer(objects=[video_object])
+    else:
+        return video_object
