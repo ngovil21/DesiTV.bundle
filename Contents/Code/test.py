aeaa166fbbde259ed002f6daec5d62012cf838ef
ngovil21 <ngovil21@gmail.com>
2015-06-14 13:06:38 -0700

    fixes

--- Contents/Code/test.py
+++ Contents/Code/test.py
@@ -12,7 +12,7 @@ TEST_URL = "http://www.videoweed.es/file/9f6fdc1064762"
 def TestMenu(url):
 
 
-  html = HTML.ElementFromURL("http://www.hindistop.com/chakravartin-ashoka-samrat-2/",headers={'Referer': "http://bollystop.com/redirector.php?r=http://www.hindistop.com/chakravartin-ashoka-samrat-2/&s=646022"])
+  html = HTML.ElementFromURL("http://www.hindistop.com/chakravartin-ashoka-samrat-2/", headers={'Referer': "http://bollystop.com/redirector.php?r=http://www.hindistop.com/chakravartin-ashoka-samrat-2/&s=646022"})
   Log(HTML.StringFromElement(html))
 
   oc = ObjectContainer(title2="Test")
