d3c6b80d76714abdd9b1ae26f81f0fd4f52508dd
ngovil21 <ngovil21@gmail.com>
2015-06-14 13:25:06 -0700

    fixes

--- Contents/Code/bollystop.py
+++ Contents/Code/bollystop.py
@@ -137,12 +137,13 @@ def EpisodeLinksMenu(url, title):
     for item in items:
         try:
             # Video site
-            # videosite = item.xpath("./text()")[0]
+            videotitle = item.xpath("./text()")[0]
             # Video link
             link = item.xpath("./@href")[0]
             if not link.startswith("http:"):
                 link = SITEURL + link
-            match = re.compile('redirector.php\?r=(.+?)&s=(.+?)').match(link)
+            Log(link)
+            match = re.compile('redirector.php\?r=(.+?)&s=(.+?)').search(link)
             redirect = match.group(0)
             Log(redirect)
             # Show date
@@ -157,13 +158,13 @@ def EpisodeLinksMenu(url, title):
             # Log ('Dailymotion Link: ' + link)
             oc.add(VideoClipObject(
                 url=link,
-                title=videosite,
+                title=videotitle,
                 thumb=Resource.ContentsOfURLWithFallback(R(ICON), fallback=R(ICON)),
                 originally_available_at=Datetime.ParseDate(date).date()))
         elif link.find('playwire') != -1:
             oc.add(CreateVideoObject(
                 url=link,
-                title=videosite,
+                title=videotitle,
                 thumb=Resource.ContentsOfURLWithFallback(R(ICON), fallback=R(ICON)),
                 originally_available_at=Datetime.ParseDate(date).date()))
 
@@ -209,3 +210,49 @@ def GetURLSource(url, referer, date=''):
 
   # return url, thumb
   return url
+
+
+@route(PREFIX + '/desirulez/createvideoobject')
+def CreateVideoObject(url, title, thumb, summary='', originally_available_at='', include_container=True):
+  try:
+    originally_available_at = Datetime.ParseDate(originally_available_at).date()
+  except:
+    originally_available_at = None
+
+  container = Container.MP4
+  video_codec = VideoCodec.H264
+  audio_codec = AudioCodec.AAC
+  audio_channels = 2
+
+  video_object = VideoClipObject(
+    key = Callback(
+      CreateVideoObject,
+      url=url,
+      title=title,
+      summary=summary,
+      thumb=thumb,
+      originally_available_at=originally_available_at,
+      include_container=True
+    ),
+    rating_key = url,
+    title = title,
+    summary=summary,
+    thumb=thumb,
+    originally_available_at=originally_available_at,
+    items = [
+      MediaObject(
+        parts = [
+          PartObject(key=url)
+        ],
+        container = container,
+        video_codec = video_codec,
+        audio_codec = audio_codec,
+        audio_channels = audio_channels
+      )
+    ]
+  )
+
+  if include_container:
+    return ObjectContainer(objects=[video_object])
+  else:
+    return video_object
