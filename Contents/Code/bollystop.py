6f76a852683ebba4eba864754f6b58e6483c4346
ngovil21 <ngovil21@gmail.com>
2015-06-14 03:36:38 -0700

    fixes

--- Contents/Code/bollystop.py
+++ Contents/Code/bollystop.py
@@ -60,7 +60,7 @@ def ShowsMenu(url, title):
     for show in shows:
         name = show.xpath("./text()")[0]
         link = show.xpath("./@href")[0]
-        link.replace('//id//', '//episodes//')
+        link.replace("id/", "episodes/")
         image = show.xpath("./../../p//img/@src")
         if image:
             if not image[0].startswith("http:"):
