10dd0c933dda272ecbcf2ed726ebefa8be36a965
ngovil21 <ngovil21@gmail.com>
2015-06-14 23:59:13 -0700

    fixes

--- Contents/Code/bollystop.py
+++ Contents/Code/bollystop.py
@@ -233,7 +233,7 @@ def GetURLSource(url, referer, date=''):
         site = HTML.ElementFromURL(url)
         source = HTML.StringFromElement(site)
         source = source.replace('|', '/')
-        file = re.compile('file: "([^"]+)""').findall(source)
+        file = re.compile('file: "([^"]+)"').findall(source)
         image = re.compile('image: "([^"]+)"').findall(source)
         if file:
             url = file[0]
