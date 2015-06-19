109d4ce8eda2299a3dbfe0c951c218b082ca75f0
ngovil21 <ngovil21@gmail.com>
2015-06-18 19:07:12 -0700

    fixes

--- Contents/Code/apnitv.py
+++ Contents/Code/apnitv.py
@@ -145,28 +145,45 @@ def EpisodeLinksMenu(url, title, index):
 
     html = HTML.ElementFromURL(url)
 
-    items = html.xpath(".//div[@id='centerblocks']/table[" + str(index) + "]/tbody/tr/td/a")
+    items = html.xpath("//div[@id='centerblocks']/table[" + str(index) + "]/tbody/tr/td/a")
+    parts=[]
     for item in items:
         link = item.xpath("@href")[0]
         code = HTML.ElementFromURL(link)
         rlink = code.xpath("//div[@id='topsource']/a/@href")[0]
-        videosource = getURLSource(rlink)
+        videosource, poster, host = getURLSource(rlink,link)
+        parts.append(videosource)
 
     # Add the found item to the collection
-        if link.find('dailymotion') != -1:
+        if host == 'dailymotion':
             # Log ('Dailymotion Link: ' + link)
             oc.add(VideoClipObject(
                 url=videosource,
-                title=videosite,
-                thumb=Resource.ContentsOfURLWithFallback(R(ICON), fallback=R(ICON)),
-                thumb=Resource.ContentsOfURLWithFallback(R(ICON), fallback=R(ICON)),
-                originally_available_at=Datetime.ParseDate(date).date()))
-        elif link.find('playwire') != -1:
+                title=videotitle,
+                thumb=thumb
+            ))
+        elif host == 'playwire':
             oc.add(CreateVideoObject(
                 url=videosource,
-                title=videosite,
-                thumb=Resource.ContentsOfURLWithFallback(R(ICON), fallback=R(ICON)),
-                originally_available_at=Datetime.ParseDate(date).date()))
+                title=videotitle,
+                thumb=thumb,
+                urls=parts
+            ))
+        elif host == 'vodlocker':
+            oc.add(videosource(
+                url=link,
+                title=videotitle,
+                thumb=thumb,
+                urls=parts
+            ))
+        elif host == 'cloudy':
+            oc.add(CreateVideoObject(
+                url=videosource,
+                title=videotitle,
+                thumb=None,
+                container='flv',
+                urls=parts
+            ))
 
     # If there are no channels, warn the user
     if len(oc) == 0:
@@ -184,12 +201,83 @@ def getVideoHost(url):
     return
 
 
-def GetURLSource(url, referer=None, date=''):
+def GetURLSource(url, referer, date=''):
     html = HTML.ElementFromURL(url=url, headers={'Referer': referer})
     string = HTML.StringFromElement(html)
+    host = ''
+    poster = ''
+    # Log(string)
+    if html.xpath("//iframe[contains(@src,'dailymotion')]"):
+        url = html.xpath("//iframe[contains(@src,'dailymotion')]/@src")[0]
+        host = 'dailymotion'
+        thumb = None
+    elif string.find('playwire') != -1:
+        # Log("pID: " + str(len(html.xpath("//script/@data-publisher-id"))) + " vID: " + str(len(html.xpath("//script/@data-video-id"))))
+        if len(html.xpath("//script/@data-publisher-id")) != 0 and len(html.xpath("//script/@data-video-id")) != 0:
+            url = 'http://cdn.playwire.com/' + html.xpath("//script/@data-publisher-id")[0] + '/video-' + date + '-' + \
+                  html.xpath("//script/@data-video-id")[0] + '.mp4'
+            host = 'playwire'
+        else:
+            # Log("JSON: " + str(html.xpath("//script/@data-config")))
+            json_obj = JSON.ObjectFromURL(html.xpath("//script/@data-config")[0])
+            # Log("JSON: " + str(json_obj))
+            # import json
+            # Log(json.dumps(json_obj,indent=4))
+            manifest = json_obj['content']['media']['f4m']
+            poster = json_obj['content']['poster']
+            # Log("Manifest: " + str(manifest))
+            content = HTTP.Request(manifest, headers={'Accept': 'text/html'}).content
+            content = content.replace('\n', '').replace('  ', '')
+            # Log("Content: " + str(content))
+            baseurl = re.search(r'>http(.*?)<', content)  # <([baseURL]+)\b[^>]*>(.*?)<\/baseURL>
+            # Log ('BaseURL: ' + baseurl.group())
+            baseurl = re.sub(r'(<|>)', "", baseurl.group())
+            # Log ('BaseURL: ' + baseurl)
+            mediaurl = re.search(r'url="(.*?)\?', content)
+            # Log ('MediaURL: ' + mediaurl.group())
+            mediaurl = re.sub(r'(url|=|\?|")', "", mediaurl.group())
+            # Log ('MediaURL: ' + mediaurl)
+            url = baseurl + "/" + mediaurl
+            host = 'playwire'
+            Log("URL: " + url)
+            thumb = poster
+    elif html.xpath("//iframe[contains(@src,'vodlocker')]"):
+        url = html.xpath("//iframe[contains(@src,'vodlocker')]/@src")[0]
+        site = HTML.ElementFromURL(url)
+        source = HTML.StringFromElement(site)
+        source = source.replace('|', '/')
+        file = re.compile('file: "([^"]+)"').findall(source)
+        image = re.compile('image: "([^"]+)"').findall(source)
+        if file:
+            url = file[0]
+            Log(url)
+            poster = image[0]
+        else:
+            return None,None,None
+        host = 'vodlocker'
+        thumb = None
+    elif html.xpath("//iframe[contains(@src,'cloudy')]"):
+        url = html.xpath("//iframe[contains(@src,'cloudy')]/@src")[0]
+        site = HTTP.Request(url)
+        file = re.compile('file:[ ]?"([^"]+)"').findall(site.content)
+        host = 'cloudy'
+        if file:
+            file_id = file[0]
+            #Log(file_id)
+            key = re.compile('key:[ ]?"([^"]+)"').findall(site.content)[0]
+            #Log(key)
+            api_call = ('http://www.cloudy.ec/api/player.api.php?user=undefined&codes=1&file=%s&pass=undefined&key=%s') % (file_id, key)
+            site = HTTP.Request(api_call)
+            content = re.compile('url=([^&]+)&').findall(site.content)
+            if content:
+                url = String.Unquote(content[0])
+                Log(url)
+                poster = None
+        else:
+            return None, None, None
 
-    videocontainer = html.xpath("//div[@class='video_container']")[0]
-    return videocontainer.xpath("./iframe/@src")
+    # return url, thumb
+    return url, host, poster
 
 
 ####################################################################################################
@@ -205,46 +293,58 @@ def URLCheck(url):
 ####################################################################################################
 
 @route(PREFIX + '/apnitv/createvideoobject')
-def CreateVideoObject(url, title, thumb, originally_available_at, include_container=False):
-    try:
-        originally_available_at = Datetime.ParseDate(originally_available_at).date()
-    except:
-        originally_available_at = None
+def CreateVideoObject(url, title, thumb=None, summary='', container='', originally_available_at=None,
+                      include_container=False,
+                      urls=None):
 
+
+try:
+    originally_available_at = Datetime.ParseDate(originally_available_at).date()
+except:
+    originally_available_at = None
+
+if not container:
     container = Container.MP4
-    video_codec = VideoCodec.H264
-    audio_codec = AudioCodec.AAC
-    audio_channels = 2
+video_codec = VideoCodec.H264
+audio_codec = AudioCodec.AAC
+audio_channels = 2
 
-    video_object = VideoClipObject(
-        key=Callback(
-            CreateVideoObject,
-            url=url,
-            title=title,
-            thumb=thumb,
-            originally_available_at=originally_available_at,
-            include_container=True
-        ),
-        rating_key=url,
+if urls:
+    parts = []
+    for part in urls:
+        parts.append(PartObject(key=part))
+else:
+    parts = [
+        PartObject(key=url)
+    ]
+
+video_object = VideoClipObject(
+    key=Callback(
+        CreateVideoObject,
+        url=url,
         title=title,
+        summary=summary,
         thumb=thumb,
         originally_available_at=originally_available_at,
-        items=[
-            MediaObject(
-                parts=[
-                    PartObject(key=url)
-                ],
-                container=container,
-                video_codec=video_codec,
-                audio_codec=audio_codec,
-                audio_channels=audio_channels
-            )
-        ]
-    )
+        include_container=True
+    ),
+    rating_key=url,
+    title=title,
+    summary=summary,
+    thumb=thumb,
+    originally_available_at=originally_available_at,
+    items=[
+        MediaObject(
+            parts=parts,
+            container=container,
+            video_codec=video_codec,
+            audio_codec=audio_codec,
+            audio_channels=audio_channels
+        )
+    ]
+)
 
-    if include_container:
-        return ObjectContainer(objects=[video_object])
-    else:
-        return video_object
-
-        ####################################################################################################
+if include_container:
+    return ObjectContainer(objects=[video_object])
+else:
+    return video_object
